90c9408ffaf7803ebf0c22ac4f2a7827dbee5d6b
Luthiery Costa <luthierycosta@gmail.com>
2020-08-01 03:32:40 -0300

    Mapeamento dos horários atualizado de acordo com o post do instagram da UnB

--- codigo.user.js
+++ codigo.user.js
@@ -23,25 +23,26 @@ const mapaDias = {
     7: 'SAB'
 }
 const mapaHorarios = {
-    'M1': {inicio: '08:00', fim: '08:50'},
-    'M2': {inicio: '09:00', fim: '09:50'},
-    'M3': {inicio: '10:00', fim: '10:50'},
-    'M4': {inicio: '11:00', fim: '11:50'},
-    'M5': {inicio: '12:00', fim: '12:50'},
-    'T1': {inicio: '13:00', fim: '13:50'},
-    'T2': {inicio: '14:00', fim: '14:50'},
-    'T3': {inicio: '15:00', fim: '15:50'},
-    'T4': {inicio: '16:00', fim: '16:50'},
-    'T5': {inicio: '17:00', fim: '17:50'},
-    'T6': {inicio: '18:00', fim: '18:50'},
+    'M1': {inicio: '08:00', fim: '08:55'},
+    'M2': {inicio: '08:55', fim: '09:50'},
+    'M3': {inicio: '10:00', fim: '10:55'},
+    'M4': {inicio: '10:55', fim: '11:50'},
+    'M5': {inicio: '12:00', fim: '12:55'},
+    'T1': {inicio: '12:55', fim: '13:50'},
+    'T2': {inicio: '14:00', fim: '14:55'},
+    'T3': {inicio: '14:55', fim: '15:50'},
+    'T4': {inicio: '16:00', fim: '16:55'},
+    'T5': {inicio: '16:55', fim: '17:50'},
+    'T6': {inicio: '18:00', fim: '18:55'},
+    'T7': {inicio: '18:55', fim: '19:50'},
     'N1': {inicio: '19:00', fim: '19:50'},
-    'N2': {inicio: '20:00', fim: '20:40'},
+    'N2': {inicio: '19:50', fim: '20:40'},
     'N3': {inicio: '20:50', fim: '21:40'},
-    'N4': {inicio: '21:50', fim: '22:30'}
+    'N4': {inicio: '21:40', fim: '22:30'}
 }
 
 /** Padrão regex que reconhece o formato de horário do SIGAA */
-const padraoSigaa = /\b([2-7]{1,5})([MTN])([1-6]{1,6})\b/gm;
+const padraoSigaa = /\b([2-7]{1,5})([MTN])([1-7]{1,7})\b/gm;
 
 /**
  * Função que recebe o horário do SIGAA e retorna o texto traduzido através do dicionário acima
@@ -62,8 +63,6 @@ function mapeiaTexto(match, g1, g2, g3) {
 }
 
 /** Objeto TreeWalker que permite navegar por todos os campos de texto da página
- * (copiado na cara dura do Stack Overflow)
- * https://stackoverflow.com/questions/39380229/replace-all-instance-of-a-string-in-a-webpage-on-click-javascript
 */
 var treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
 
@@ -83,7 +82,7 @@ if (document.getElementById("turmas-portal") != null) { // nesse caso a página 
     .children[2]                        // acessa a coluna horário
     .width = "18%";   
 }
-else if (document.getElementsByClassName("listagem") != undefined) { // nesse caso a página é a das turmas anteriores
+else if (document.getElementsByClassName("listagem") != undefined) { // nesse caso é uma das páginas abaixo
     let url = window.location.href;
     let colunas = document.getElementsByClassName("listagem")[0].tHead.children[0].children;
     for (var coluna of colunas) {
