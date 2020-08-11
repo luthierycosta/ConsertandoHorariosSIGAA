fd6fe72c6ec0c45fe204171925b347239905bad5
Luthiery Costa <luthierycosta@gmail.com>
2020-08-10 22:00:04 -0300

    Separa, junta e ordena. Será se funciona isso?

--- codigo.user.js
+++ codigo.user.js
@@ -71,10 +71,45 @@ function separaDias(match, g1, g2, g3) {
 }
 
 function ordenaDias(texto) {
-    return [...texto.matchAll(padraoSigaa)]
-        .sort((a,b) =>  a[1] < b[1] ? -1 :
-                        a[1] > b[1] ? 1 : 0)
-        .map(match => match[0])
+
+    let horarios = [];
+    for (horario of [...texto.matchAll(padraoSigaa)])
+        for (dia of horario[1])
+            horarios.push(
+                [`${dia}`,
+                 `${horario[2]}`,
+                 `${horario[3]}`]
+            );
+
+    let turnos_juntos = [];
+    let visitado = [];
+    
+    for (let i = 0; i < horarios.length; i++) {   
+        if (visitado[i])
+            continue;
+        
+        for (let j = i+1; j < horarios.length; j++) {
+            if (horarios[i][0] == horarios[j][0]) {
+                turnos_juntos.push(
+                    [`${horarios[i][0]}`,
+                     `${horarios[i][1]}${horarios[j][1]}`,
+                     `${horarios[i][2]}${horarios[j][2]}`]
+                );
+            } else {
+                turnos_juntos.push(
+                    [`${horarios[i][0]}`,
+                     `${horarios[i][1]}`,
+                     `${horarios[i][2]}`]
+                );
+            }
+            visitado[j] = true;
+        } 
+    }
+    
+    return turnos_juntos
+        .sort((a,b) =>  a[0] < b[0] ? -1 :
+                        a[0] > b[0] ? 1 : 0)
+        .map((match) => `${match[0]}${match[1]}${match[2]}`)
         .join(' ');
 }
 
@@ -91,7 +126,6 @@ const treeWalker = document.createTreeWalker(
 /** Procura por todos os textos da página e, onde reconhecer o padrão de horário, chama a mapeiaHorarios() */
 let node;
 while(node = treeWalker.nextNode()){
-    node.textContent = node.textContent.replace(padraoSigaa,separaDias);
     node.textContent = ordenaDias(node.textContent);
     node.textContent = node.textContent.replace(padraoSigaa,mapeiaHorarios);
 }
